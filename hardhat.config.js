d025cd6307b28849e0293533664b42c5817a4f76
Yash <72552910+kumaryash90@users.noreply.github.com>
2024-07-12 20:15:32 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 const TESTNET_PRIVATE_KEY =
-    process.env.TESTNET_PRIVATE_KEY || "";
+    process.env.TESTNET_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
 module.exports = {
   solidity: "0.8.3",
   networks: {
