c5895e4eb89809d6aafed1e00b19e5ddfeddd6ad
Yash <72552910+kumaryash90@users.noreply.github.com>
2024-07-12 20:13:55 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,18 @@
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-
+const TESTNET_PRIVATE_KEY =
+    process.env.TESTNET_PRIVATE_KEY || "";
 module.exports = {
   solidity: "0.8.3",
   networks: {
     rinkeby: {
-      url: process.env.ALCHEMY_RINKEBY,
-      accounts: [process.env.ADMIN_PRIVATE_KEY]
+      url: process.env.ALCHEMY_RINKEBY || "",
+      accounts: [TESTNET_PRIVATE_KEY]
     },
     ropsten: {
-      url: process.env.ALCHEMY_ROPSTEN,
-      accounts: [process.env.ADMIN_PRIVATE_KEY]
+      url: process.env.ALCHEMY_ROPSTEN || "",
+      accounts: [TESTNET_PRIVATE_KEY]
     }
   },
 };
